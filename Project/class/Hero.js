8b260b2388fb111b01bf3e5530ff1d3ffabd0300
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-22 13:22:13 +0700

    Create Hero.js

---
+++ Project/class/Hero.js
@@ -0,0 +1,19 @@
+class Hero{
+  
+  constructor(life, score){
+    this.life = life;
+    this.score = score;
+  }
+  
+  increaseScore(){
+    
+  }
+  
+  calculateLife(){
+    
+  }
+  
+  saveScore(){
+    
+  }
+}
