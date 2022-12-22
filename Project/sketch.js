81f04eb232f6e716151c8d1e3578bebb1b5373c1
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-23 02:27:16 +0700

    Update sketch.js

--- Project/sketch.js
+++ Project/sketch.js
@@ -1,10 +1,11 @@
 let bullets = [];
-let velocities = [];
 
+//setup
 function setup(){
   createCanvas(550,500);
 }
 
+//continously
 function draw(){
   background(220);
   
@@ -14,31 +15,28 @@ function draw(){
   //draw line between center circle and player
   line(width/2, height/2,mouseX, mouseY)
   
+  //shoot bullet
   for (let bullet of bullets){
-    for (let velocity of velocities){
-      circle(bullet.x, bullet.y, 10);
-      bullet.x += bullet.Vx * velocity.x;
-      bullet.y += bullet.Vy * velocity.y;
-    }
+    circle(bullet.x, bullet.y, 10);
+    bullet.x += bullet.Vx * bullet.Ax;
+    bullet.y += bullet.Vy * bullet.Ay;
   }
 }
 
 function mousePressed(){
+  //define the angle
+  const angle = Math.atan2(mouseY - height/2, mouseX - width/2);
+  
+  //bullet attribut
   let bullet = {
     x: width/2,
     y: height/2,
     Vx: 4,
     Vy: 4,
+    Ax: Math.cos(angle),
+    Ay: Math.sin(angle)
   }
-  bullets.pop(bullet);
+  
+  //push bullet to bullets array
   bullets.push(bullet);  
-  
-  const angle = Math.atan2(mouseY - height/2, mouseX - width/2);
-  
-  const velocity = {
-    x: Math.cos(angle),
-    y: Math.sin(angle)
-  }
-  velocities.pop(velocity);
-  velocities.push(velocity);
 }
