139fbda3c92a20f3fd827d3bdc407496f4154ac9
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-22 13:41:32 +0700

    Update sketch.js

--- Project/sketch.js
+++ Project/sketch.js
@@ -1,7 +1,24 @@
+let yPos = 0;
 function setup() {
-  createCanvas(400, 400);
+  // create canvas (width,height)
+  createCanvas(550,500)
+  // setup() runs once
+  frameRate(80);
+}
+function draw() {
+  // draw() loops forever, until stopped
+  background(204);
+  yPos = yPos - 1;
+  if (yPos < 0) {
+    yPos = height;
+  }
+  line(0, yPos, width, yPos);
 }
 
-function draw() {
-  background(220);
+function mousePressed() {
+  noLoop();
+}
+
+function mouseReleased() {
+  loop();
 }
