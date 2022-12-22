a87e535256342ac1216d5bcfd12a5431225455cb
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-23 02:02:16 +0700

    Update sketch.js

--- Project/sketch.js
+++ Project/sketch.js
@@ -1,24 +1,43 @@
-let yPos = 0;
-function setup() {
-  // create canvas (width,height)
-  createCanvas(550,500)
-  // setup() runs once
-  frameRate(80);
+let bullets = [];
+let velocities = [];
+
+function setup(){
+  createCanvas(550,500);
 }
-function draw() {
-  // draw() loops forever, until stopped
-  background(204);
-  yPos = yPos - 1;
-  if (yPos < 0) {
-    yPos = height;
+
+function draw(){
+  background(220);
+  
+  //drawcenter circle
+  circle(width/2, height/2, 40);
+  
+  //draw line between center circle and player
+  line(width/2, height/2,mouseX, mouseY)
+  
+  for (let bullet of bullets){
+    for (let velocity of velocities){
+      circle(bullet.x, bullet.y, 10);
+      bullet.x += bullet.Vx * velocity.x;
+      bullet.y += bullet.Vy * velocity.y;
+    }
   }
-  line(0, yPos, width, yPos);
 }
 
-function mousePressed() {
-  noLoop();
-}
-
-function mouseReleased() {
-  loop();
+function mousePressed(){
+  let bullet = {
+    x: width/2,
+    y: height/2,
+    Vx: 4,
+    Vy: 4,
+  }
+  bullets.push(bullet);  
+  
+  const angle = Math.atan2(mouseY - height/2, mouseX - width/2);
+  
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  velocities.pop(velocity);
+  velocities.push(velocity);
 }
