c5b86f03728686b73c4b1128c4e407ef61077171
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-22 13:21:22 +0700

    Create Entity.js

---
+++ Project/class/Entity.js
@@ -0,0 +1,29 @@
+class Entity{
+  
+  constructor(height, width, x, y){
+    this.height = height;
+    this.width = width;
+    this.x = x;
+    this.y= y;
+  }
+  
+  attack(){
+    
+  }
+  
+  moveRight(){
+    
+  }
+  
+  moveLeft(){
+    
+  }
+  
+  moveDown(){
+    
+  }
+  
+  moveUp(){
+    
+  }
+}
