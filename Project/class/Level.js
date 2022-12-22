bdb01798ddad61374dbbe486dc26bacb9c0373a1
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-22 13:20:47 +0700

    Create Level.js

---
+++ Project/class/Level.js
@@ -0,0 +1,16 @@
+class Level{
+  
+  constructor(currentLevel, latestLevel, maxLevel){
+    this.currentLevel = currentLevel;
+    this.latestLevel = latestLevel;
+    this.maxLevel = maxLevel;
+  }
+  
+  setLevel(){
+    
+  }
+  
+  getCurrentLevel(){
+    
+  }
+}
