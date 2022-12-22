566af56a948b0cca99fb1166eea259d4855cb60b
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2022-12-22 13:22:41 +0700

    Create Monsters.js

---
+++ Project/class/Monsters.js
@@ -0,0 +1,17 @@
+class Monsters{
+  
+  constructor(life, color, effect, type){
+    this.life = life;
+    this.color = color;
+    this.effect = effect;
+    this.type = type;
+  }
+  
+  moveRandom(){
+    
+  }
+  
+  saveScore(){
+    
+  }
+}
