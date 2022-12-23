7be26d55c4d62cbd000363b5979ad744ecfc412c
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-12-23 18:22:10 +0700

    Update index.js

--- Project/Prototype/index.js
+++ Project/Prototype/index.js
@@ -4,3 +4,33 @@ const ctx = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Entity {
+  
+    constructor(height, width, x, y){
+      this.height = height;
+      this.width = width;
+      this.x = x;
+      this.y= y;
+    }
+    
+    attack(){
+      
+    }
+    
+    moveRight(){
+      
+    }
+    
+    moveLeft(){
+      
+    }
+    
+    moveDown(){
+      
+    }
+    
+    moveUp(){
+      
+    }
+    
+}
