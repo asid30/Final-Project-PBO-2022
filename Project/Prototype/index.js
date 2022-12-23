131fb36ac419be77ffac4f6427f1ac1e8b881422
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-12-23 18:32:10 +0700

    Update index.js

--- Project/Prototype/index.js
+++ Project/Prototype/index.js
@@ -58,13 +58,16 @@ class Hero extends Entity {
 
     draw(){
         ctx.beginPath();
-        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.arc(this.x, this.y, this.height+this.width/2, 0, Math.PI*2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
     }
 
 }
 
-const player = new Hero(30,30,cxCenter,cyCenter,'blue',100,0);
+const cxCenter = canvas.width / 2;
+const cyCenter = canvas.height / 2;
+
+const player = new Hero(20,20,cxCenter,cyCenter,'blue',100,0);
 player.draw();
 console.log(player);
