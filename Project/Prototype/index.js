ca7e3fe72f786e9a99d910aa6af520fb324e9254
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-12-23 18:33:46 +0700

    Update index.js

--- Project/Prototype/index.js
+++ Project/Prototype/index.js
@@ -65,6 +65,29 @@ class Hero extends Entity {
 
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const cxCenter = canvas.width / 2;
 const cyCenter = canvas.height / 2;
 
