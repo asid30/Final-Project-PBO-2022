eac920f2e5d0873a2c6a892912c73b44099b55c8
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-12-24 11:10:32 +0700

    Update index.js

--- Project/Prototype/index.js
+++ Project/Prototype/index.js
@@ -127,7 +127,7 @@ class Projectile {
 const cxCenter = canvas.width / 2;
 const cyCenter = canvas.height / 2;
 
-const player = new Hero(20,20,cxCenter,cyCenter,20,'blue',100,0);
+const player = new Hero(20,20,cxCenter,cyCenter,20,'white',100,0);
 player.draw();
 console.log(player);
 
@@ -149,7 +149,7 @@ function spawnMonster(){
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
         }
 
-        const color = 'green';
+        const color = `hsl(${Math.random() * 360}, 100%, 50%)`;
         const angle = Math.atan2(canvas.height/2 - y, canvas.width/2 - x);
         const velocity = {x : Math.cos(angle), y : Math.sin(angle)};
 
@@ -159,9 +159,17 @@ function spawnMonster(){
 let animationId;
 function animate(){
     animationId = requestAnimationFrame(animate);
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0,1)';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
     player.draw();
-    projectiles.forEach(projectile => {projectile.update()})
+    projectiles.forEach((projectile, index) => {
+        projectile.update();
+        if(projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height){
+            setTimeout(() => {
+                projectiles.splice(index,1);
+            }, 0)
+        }
+    })
 
     monsters.forEach((monster,index) => {
         monster.update()
@@ -185,7 +193,7 @@ function animate(){
 window.addEventListener('click', (event) => {
         const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2);
         const velocity = {x : Math.cos(angle)*5, y : Math.sin(angle)*5}
-        projectiles.push(new Projectile(canvas.width/2,canvas.height/2,5,'red',velocity))
+        projectiles.push(new Projectile(canvas.width/2,canvas.height/2,5,'white',velocity))
     })
 
 animate();
